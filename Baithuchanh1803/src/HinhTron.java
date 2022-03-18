416877818d4e90adc637cf012389574ed58c7b68
ngocthan27 <baybaobonso9@gmail.com>
2022-03-18 21:52:21 +0700

    commit

--- Baithuchanh1803/src/HinhTron.java
+++ Baithuchanh1803/src/HinhTron.java
@@ -1,5 +1,36 @@
-public class App {
+import java.util.Scanner;
+public class HinhTron {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    }
+
+    final float PI = 3.14f;
+    float r;
+    float cv;
+    float dt;
+    void NhapBanKinh()
+    {
+        System.out.print("Nhap vao ban kinh hinh tron: ");
+        Scanner sc = new Scanner(System.in);
+        r = sc.nextFloat();
+        sc.close();
+    }
+
+    void TinhChuVi()
+    {
+        cv = 2 * PI * r;
+    }
+
+    void TinhDienTinh()
+    {
+        dt = PI * r * r;
+    }
+    void inChuVi()
+    {
+        System.out.print("Chu vi hinh tron: " + cv);
+    }
+    void inDienTich()
+    {
+        System.out.print("Dien tich hinh tron: " + dt);
     }
 }
